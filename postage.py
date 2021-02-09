7c5c778f43af804a9477d9e0ee3f7240d183fee0
Taylor Cooke <tcooke@relias.com>
2021-02-09 15:38:05 -0500

    polished up. may be good to go

--- postage.py
+++ postage.py
@@ -8,8 +8,13 @@ dt_string = now.strftime("%d-%m-%Y_%H-%M-%S")
 postage = pd.read_excel('Postage_stripped.xlsx', sheet_name='Endicia')
 shipments = pd.read_excel('Shipments.xlsx', sheet_name='SH_Shipments')
 
+tracking = pd.DataFrame({'Tracking Number': shipments['Tracking Number'], 'Customer': shipments['3PL Customer']})
+universal = postage[postage['Unique ID Type'] == 'account_balance_id:postage_id_universal']
+
+
 # Postage BA file is col 53. Is inserted into 54
 # Shipments searching col 11, returning what's in column 17
 
-new_postage = postage.merge(shipments[['Tracking Number', '3PL Customer']], on='Tracking Number')
+new_postage = pd.merge(universal, tracking, on='Tracking Number')
+del new_postage['Unnamed: 53']
 new_postage.to_excel(f'testPostage_{dt_string}.xlsx')
