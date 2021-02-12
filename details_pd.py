8d3fb73c660c669aa975473141e85cab1806b8e5
Taylor Cooke <tcooke@relias.com>
2021-02-11 21:05:38 -0500

    working well with test file

--- details_pd.py
+++ details_pd.py
@@ -9,46 +9,39 @@ now = datetime.now()
 dt_string = now.strftime("%d-%m-%Y_%H-%M-%S")
 
 wb = pd.ExcelFile('Client Detail_2021_2H_concat_slim_pandas.xlsx', engine='openpyxl')
-worksheets = wb.sheet_names # gets sheet names - works
+worksheets = wb.sheet_names # gets sheet names - works but also creates sheet named ' (200)  Storage' that needs to be deleted
 
-# trying to print data for client
-# client = 'Kammok'
-# df = pd.read_excel(wb, 'FedEx', header=0)
-# df = df.where(df['3PL Client']==client)
-# df = df.dropna()
-# print(df.head(10))
+del worksheets[0]
+
 
 # gets clients from weekly pallet counts - works
-# wpc_df = pd.read_excel(wb, 'Weekly Pallet Counts')
-# clients = wpc_df['concat'].unique()
+wpc_df = pd.read_excel(wb, 'Weekly Pallet Counts')
+clients = wpc_df['concat'].unique()
 # print(clients)
 
-fedex = pd.read_excel(wb, 'FedEx', header=0)
-usps = pd.read_excel(wb, 'USPS', header=0)
-howler_fed = fedex[fedex['concat'] == 'Howler Brothers']
-howler_usps = usps[usps['concat'] == 'Howler Brothers']
+# fedex = pd.read_excel(wb, 'FedEx', header=0)
+# usps = pd.read_excel(wb, 'USPS', header=0)
+# howler_fed = fedex[fedex['concat'] == 'Howler Brothers']
+# howler_usps = usps[usps['concat'] == 'Howler Brothers']
 
 # kammok.to_excel(f'testDetails_{dt_string}.xlsx', sheet_name='Fedex')
 
-# writing multiple sheets:
-with pd.ExcelWriter(f'testDetails_{dt_string}.xlsx') as writer:  # pylint: disable=abstract-class-instantiated
-    howler_fed.to_excel(writer, sheet_name='Fedex')
-    howler_usps.to_excel(writer, sheet_name='usps')
+# writing multiple sheets: - works
+# with pd.ExcelWriter(f'testDetails_{dt_string}.xlsx') as writer:  # pylint: disable=abstract-class-instantiated
+#     howler_fed.to_excel(writer, sheet_name='Fedex')
+#     howler_usps.to_excel(writer, sheet_name='usps')
+testing_clients = ['Howler Brothers', 'Rowing Blazers', 'Trek Light Gear', 'William Murray Golf']
 
-# for sheet in worksheets:
-#     client = 'Airhouse'
-#     df = pd.read_excel(spreadsheet, 'FedEx', header=0)
-#     df = df.where(df['3PL Client']==client)
-#     df = df.dropna()
-#     print(df.head(10))
-
-# ## pandas to create the files
-# excel_file = 'ClientTest.xlsx'
-
-# def open_excel(filename):
-#     df = pd.read_excel(filename)
-#     return df
-
+for client in testing_clients:
+    print(client)
+    writer = ExcelWriter(f'{client}_Details_{dt_string}.xlsx')
+    for sheet in worksheets:
+        print(sheet)
+        sheet_frame = pd.read_excel(wb, sheet, header=0)
+        client_sheet = sheet_frame[sheet_frame['concat'] == client]
+        # with pd.ExcelWriter(f'{client}_Details_{dt_string}.xlsx') as writer:  # pylint: disable=abstract-class-instantiated
+        client_sheet.to_excel(writer, sheet_name=sheet)
+    writer.save()
 
 # def split_data(raw_data):
 #     df_processed = pd.DataFrame(raw_data)
